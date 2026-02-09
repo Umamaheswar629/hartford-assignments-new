565b6bcfbe574f247e55976e4170a81628ead66c
Umamaheswar629 <umamaheswarreddy2113@gmail.com>
2026-02-09 10:12:09 +0530

    Add files via upload

---
+++ Week-6/Requirement-6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Requirement_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of vehicles");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Vehicle> vehicleList = new List<Vehicle>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string details = Console.ReadLine();
+                vehicleList.Add(Vehicle.CreateVehicle(details));
+            }
+
+            SortedDictionary<string, int> result =
+                Vehicle.TypeWiseCount(vehicleList);
+
+            Console.Write("{0,-15} {1}\n", "Type", "No. of Vehicles");
+
+            foreach (var entry in result)
+            {
+                Console.WriteLine("{0,-15} {1}", entry.Key, entry.Value);
+            }
+        }
+    }
+}
---
+++ Week-6/Requirement-6/Vehicle.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Requirement_6
+{
+    public class Vehicle
+    {
+        private string registrationNo;
+        private string name;
+        private string type;
+        private double weight;
+
+        public string RegistrationNo
+        {
+            get { return registrationNo; }
+            set { registrationNo = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public Vehicle() { }
+
+        public Vehicle(string registrationNo, string name, string type, double weight)
+        {
+            this.registrationNo = registrationNo;
+            this.name = name;
+            this.type = type;
+            this.weight = weight;
+        }
+
+        public static Vehicle CreateVehicle(string detail)
+        {
+            string[] data = detail.Split(',');
+
+            return new Vehicle(
+                data[0].Trim(),
+                data[1].Trim(),
+                data[2].Trim(),
+                double.Parse(data[3])
+            );
+        }
+
+        public static SortedDictionary<string, int> TypeWiseCount(List<Vehicle> vehicleList)
+        {
+            SortedDictionary<string, int> result =
+                new SortedDictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+
+            foreach (Vehicle v in vehicleList)
+            {
+                if (result.ContainsKey(v.Type))
+                {
+                    result[v.Type]++;
+                }
+                else
+                {
+                    result[v.Type] = 1;
+                }
+            }
+            return result;
+        }
+    }
+}
